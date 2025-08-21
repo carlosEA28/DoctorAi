2419040c277f8991214e267c89a09c7703ff7bb2
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-21 16:50:25 -0300

    feat: add custom error message for existing user

--- src/app/authentication/components/signup-form.tsx
+++ src/app/authentication/components/signup-form.tsx
@@ -22,6 +22,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const registerSchema = z.object({
   name: z.string().trim().min(1, { message: "O nome é obrigatório" }),
@@ -55,6 +56,14 @@ const SignupFormComponent = () => {
       {
         onSuccess: () => {
           router.push("/dashboard");
+        },
+        onError: (ctx) => {
+          if (ctx.error.code === "USER_ALREADY_EXISTS") {
+            toast.error("Email já cadastrado");
+            return;
+          }
+
+          toast.error("Erro ao criar a conta");
         },
       },
     );
