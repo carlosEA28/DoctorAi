8a6eba20518b04b4c9c4b62eff9838e1e3d541f0
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-25 13:34:27 -0300

    feat: add date-picker component with nuqs integration

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,9 +1,21 @@
+import {
+  PageAction,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+import { db } from "@/db";
+import { patientsTable } from "../../../db/schema";
+import { eq } from "drizzle-orm";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import SignoutButtonComponent from "./_components/signOutButton";
 import { redirect } from "next/navigation";
+import { DatePicker } from "./_components/data-picker";
 
-const DashboardPage = async () => {
+export default async function PatientsPage() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -15,14 +27,26 @@ const DashboardPage = async () => {
   if (!session?.user.clinic) {
     redirect("/clinic-form");
   }
+  const patients = await db.query.patientsTable.findMany({
+    where: eq(patientsTable.clinicId, session.user.clinic.id),
+  });
 
   return (
-    <div>
-      <h1>dashboard</h1>
-      <h1>{session?.user?.name}</h1>
-      <SignoutButtonComponent />
-    </div>
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Pacientes</PageTitle>
+          <PageDescription>
+            Gerencie os pacientes da sua clínica
+          </PageDescription>
+        </PageHeaderContent>
+        <PageAction>
+          <DatePicker />
+        </PageAction>
+      </PageHeader>
+      <PageContent>
+        <></>
+      </PageContent>
+    </PageContainer>
   );
-};
-
-export default DashboardPage;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { ReactQueryProvider } from "../../react-query";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
@@ -26,10 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+      <body className={`${manrope.variable} antialiased`}>
+        <ReactQueryProvider>
+          <NuqsAdapter> {children}</NuqsAdapter>
+        </ReactQueryProvider>
         <Toaster position="bottom-center" richColors theme="system" />
       </body>
     </html>
